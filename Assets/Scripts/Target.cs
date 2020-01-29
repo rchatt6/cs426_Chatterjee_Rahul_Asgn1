b49b6b6629a33fae7a22572b273df716430bbe59
rchatt6 <rahulchat0007@gmail.com>
2020-01-28 22:08:29 -0600

    whatever

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public Score scoreManager;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        scoreManager.AddPoint();
+
+        Debug.Log("Collision Detected");
+        Destroy(gameObject);
+    }
+}
